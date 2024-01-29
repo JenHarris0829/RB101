b9d0b91644c9047f4dc2c9a97b79b6df279f0e15
Jennifer Harris <129815236+JenHarris0829@users.noreply.github.com>
2024-01-29 02:43:57 +0000

    Revised calc_bonus files

--- lesson_2/calc_bonus.rb
+++ lesson_2/calc_bonus.rb
@@ -3,17 +3,44 @@ LANGUAGE = 'en'
 require 'yaml'
 MESSAGES = YAML.load_file('calc_bonus.yml')
 
+# Methods
 def messages(message, lang = 'en')
   MESSAGES[lang][message]
 end
 
-def prompt(key)
+def language(key)
   message = messages(key, LANGUAGE)
   puts("=> #{message}")
 end
 
 def prompt(message)
   puts("=> #{message}")
+end
+
+def clear
+  system('clear')
+end
+
+def get_name
+  loop do
+    name = gets.chomp.strip
+    if name.empty?
+      prompt(messages('valid_name', LANGUAGE))
+    else
+      return name
+    end
+  end
+end
+
+def get_number
+  number = ' '
+  loop do
+    number = gets.chomp.strip
+
+    break if valid_int_or_float?(number)
+  end
+
+  number.to_f
 end
 
 def valid_number?(num)
@@ -28,64 +55,46 @@ def float?(input)
   input.to_f.to_s == input
 end
 
-def valid_number?(input)
+def valid_int_or_float?(input)
   integer?(input) || float?(input)
 end
 
-def operation_to_message(op)
-  word = case op
-              when '1'
-                prompt(messages('op1', LANGUAGE))
-              when '2'
-                prompt(messages('op2', LANGUAGE))
-              when '3'
-                prompt(messages('op3', LANGUAGE))
-              when '4'
-                prompt(messages('op4', LANGUAGE))
-              end
-  word
+def calculate(operator, number1, number2)
+  case operator
+  when '1' then number1.to_f + number2.to_f
+  when '2' then number1.to_f - number2.to_f
+  when '3' then number1.to_f * number2.to_f
+  when '4' then number1.to_f / number2.to_f
+  end
 end
 
+def operation_to_message(op)
+  case op
+  when '1'
+    prompt(messages('op1', LANGUAGE))
+  when '2'
+    prompt(messages('op2', LANGUAGE))
+  when '3'
+    prompt(messages('op3', LANGUAGE))
+  when '4'
+    prompt(messages('op4', LANGUAGE))
+  end
+end
+
+# Start of Calculator
 prompt(messages('welcome', LANGUAGE))
 
-name = ''
-loop do
-  name = gets.chomp.strip
-
-  if name.empty?
-    prompt(messages('valid_name', LANGUAGE))
-  else
-    break
-  end
-end
-
-prompt(messages('hello', LANGUAGE) + "#{name}!")
+name = get_name
+prompt(messages('hello', LANGUAGE) + name.to_s + '!')
 
 loop do
-  number1 = ''
-  loop do
-    prompt(messages('first_number', LANGUAGE))
-    number1 = gets.chomp.strip
+  prompt(messages('first_number', LANGUAGE))
+  number1 = get_number
 
-    if valid_number?(number1)
-      break
-    else
-      prompt(messages('valid_number', LANGUAGE))
-    end
-  end
+  prompt(messages('second_number', LANGUAGE))
+  number2 = get_number
 
-  number2 = ''
-  loop do
-    prompt(messages('second_number', LANGUAGE))
-    number2 = gets.chomp.strip
-
-    if valid_number?(number2)
-      break
-    else
-      prompt(messages('valid_number', LANGUAGE))
-    end
-  end
-
+  clear
 
   prompt(messages('operator', LANGUAGE))
 
@@ -102,23 +111,14 @@ loop do
 
   prompt(operation_to_message(operator))
 
-
-    result =  case operator
-              when '1'
-                number1.to_f + number2.to_f
-              when '2'
-                number1.to_f - number2.to_f
-              when '3'
-                number1.to_f * number2.to_f
-              when '4'
-                number1.to_f / number2.to_f
-              end
-
-  prompt(messages('result', LANGUAGE) + "#{result}")
+  result = calculate(operator, number1, number2)
+  prompt(messages('result', LANGUAGE) + result.to_s)
 
   prompt(messages('go_again', LANGUAGE))
   answer = gets.chomp
-  break unless answer.downcase().start_with?('y')
+  break unless answer.downcase().start_with?('y' || 's')
+
+  clear
 end
 
 prompt(messages('goodbye', LANGUAGE))
